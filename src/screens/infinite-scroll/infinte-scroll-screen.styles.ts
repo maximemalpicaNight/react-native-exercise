bd82fac4f5849e7f9cb45fae7385a40f4484ee25
maximemalpicaNight <maxime.malpica@nightborn.com>
2025-12-23 16:59:11 +0100

    Feat: Infinite scroll exercise (#2)
    * feat: feed list
    
    * feat: social-feed
    
    * feat: infinite scroll exercise
    
    * fix: infinite scroll
    
    * refactor: remove react-native-unistyles

---
+++ src/screens/infinite-scroll/infinte-scroll-screen.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    globalContainer: {
+        flex:1,
+        backgroundColor: "white"
+    },
+    flashListContainer: {
+        flex:1
+    },
+})
