6a64934ed6e9fe4d82dc5ae8deaf258303441fdd
maximemalpicaNight <maxime.malpica@nightborn.com>
2025-12-23 14:45:40 +0100

    refactor (feed-card) : types and styles files

--- src/screens/feed/components/feed-card/feed-card.tsx
+++ src/screens/feed/components/feed-card/feed-card.tsx
@@ -1,16 +1,18 @@
 import { Image } from "expo-image";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { styles } from "./feed-card.styles";
+import { FeedCardProps } from "./feed-card.types";
 
-export default function FeedCard({username, profilePicture, image, post}: {username: string, profilePicture:string, image:string, post:string}) {
+export default function FeedCard({username, profilePicture, image, post}: FeedCardProps) {
 
     return (
         <View style={styles.container}>
             <View style={styles.userContainer} >
-                <Image source={profilePicture} style={{width: 50, resizeMode: "cover", height: 50, borderRadius: 99} } />
+                <Image source={profilePicture} style={styles.imageProfilePicture} />
                 <Text style={styles.text}>{username}</Text>
             </View>
             <View style={styles.imageContainer}>
-                <Image source={image} style={{aspectRatio: 1/1 , resizeMode: "cover"} } />
+                <Image source={image} style={styles.imagePost} />
             </View>
             <View>
                 <Text style={styles.post}>{post}</Text>
@@ -20,32 +22,3 @@ export default function FeedCard({username, profilePicture, image, post}: {usern
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        paddingVertical: 20,
-        paddingHorizontal: 10,
-    },
-    userContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 10,
-    },
-    imageContainer: {
-        flex:1,
-        padding: 4,
-    },
-    text: {
-        color: "black",
-        fontWeight: "bold",
-        fontSize: 20,
-    },
-    post : {
-        fontSize: 18,
-        paddingVertical: 14,
-    },
-    divider: {
-        borderBottomColor: "#d0d0d0",
-        borderBottomWidth: 1,
-    }
-})
